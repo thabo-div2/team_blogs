2c50b8a491b39cc8d4504f1cc0343e684457e46f
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2021-10-26 15:55:51 +0200

    sign in auth done

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,25 +2,31 @@
   <form @submit.prevent="handleSubmit">
     <input type="email" required placeholder="email" v-model="email" />
     <input type="password" required placeholder="password" v-model="password" />
+    <div class="error">{{ error }}</div>
     <button>Login</button>
   </form>
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
+import useLogin from "../composables/useLogin";
+
 export default {
   setup() {
     // refs
-    const displayName = ref("");
-    const surname = ref("");
     const email = ref("");
     const password = ref("");
 
-    const handleSubmit = () => {
-      console.log(email, password);
+    const { error, login } = useLogin();
+
+    const handleSubmit = async () => {
+      await login(email.value, password.value);
+      if (!error.value) {
+        console.log("user logged in");
+      }
     };
 
-    return { email, password, handleSubmit };
+    return { email, password, handleSubmit, error };
   },
 };
 </script>
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -4,25 +4,31 @@
     <input type="text" required placeholder="surname" v-model="surname" />
     <input type="email" required placeholder="email" v-model="email" />
     <input type="password" required placeholder="password" v-model="password" />
+    <div class="error">{{ error }}</div>
     <button>Sign up</button>
   </form>
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
+import useSignup from "../composables/useSignup";
+
 export default {
   setup() {
+    const { error, signup } = useSignup();
+
     // refs
     const displayName = ref("");
     const surname = ref("");
     const email = ref("");
     const password = ref("");
 
-    const handleSubmit = () => {
-      console.log(displayName, surname, email, password);
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value);
+      console.log("user signed up");
     };
 
-    return { displayName, surname, email, password, handleSubmit };
+    return { displayName, surname, email, password, handleSubmit, error };
   },
 };
 </script>
